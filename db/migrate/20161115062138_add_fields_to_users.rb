0b10970190a6d70a29566b6f22863c6112d7f60b
unnni <r.unnikrishnan@outlook.com>
2016-11-16 14:55:36 +0530

    fixed typo

--- db/migrate/20161115062138_add_fields_to_users.rb
+++ db/migrate/20161115062138_add_fields_to_users.rb
@@ -1,6 +1,6 @@
 class AddFieldsToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :first_name, :sstring
+    add_column :users, :first_name, :string
     add_column :users, :last_name, :string
   end
 end
